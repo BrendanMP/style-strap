f4e7495d4afdddd4c3e3164b38b869809618ac66
brendanmp <brendan.m.pete@gmail.com>
2020-06-01 12:57:34 -0400

    Setup custom bootstrap scss build.

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App container">
+      <header className="App__header">
+        <h1 className="py-5">App Header</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
